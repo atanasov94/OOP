688e2c1081b71157e4737bef051d31c1e4ae9aff
Atanas Atanasov <57113768+atanasov94@users.noreply.github.com>
2019-11-02 15:35:16 +0200

    Add files via upload

---
+++ singleinheritance/Animal.java
@@ -0,0 +1,10 @@
+package singleinheritance;
+
+public class Animal {
+    public Animal() {
+    }
+
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ singleinheritance/Dog.java
@@ -0,0 +1,10 @@
+package singleinheritance;
+
+public class Dog extends Animal {
+    public Dog() {
+    }
+
+    public void bark() {
+        System.out.println("barking...");
+    }
+}
