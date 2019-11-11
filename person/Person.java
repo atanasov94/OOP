9a7cd4868aa2c46aaceddf05990ea41afd00cf96
Atanas Atanasov <57113768+atanasov94@users.noreply.github.com>
2019-11-11 21:12:57 +0200

    Add files via upload

--- person/Person.java
+++ person/Person.java
@@ -1,19 +1,6 @@
 package person;
 
-public class Person {
-    private String name;
-    private int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
+public interface Person {
+    String getName();
+    int getAge();
 }
